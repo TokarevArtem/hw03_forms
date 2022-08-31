adffa3cff41c27efe38879aaf962c82942c4c21c
Артём Токарев <artemabushahmin2000@gmail.com>
2022-08-31 15:49:28 +0500

    dz2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -54,7 +54,6 @@ def post_detail(request, post_id):
 
 @login_required
 def post_create(request):
-    user = get_object_or_404(User, username=request.user)
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
