4b3e7f06877ec190274d5b3399e7dad3712ece61
Артём Токарев <artemabushahmin2000@gmail.com>
2022-11-11 08:44:05 +0500

    dz

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -33,7 +33,7 @@ def group_posts(request, slug):
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    posts = author.posts.select_related('group')
+    posts = author.posts.select_related('author')
     paginator = Paginator(posts, POSTS_NUMBER)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
